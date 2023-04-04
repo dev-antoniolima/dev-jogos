ce1ba4f35ef5aaec8ba54d27a7466b96a935f7a8
Antonio Lima <antonio.lima@metatron.com.br>
2023-04-03 22:05:15 -0300

    04 - Variáveis no java

--- main.java
+++ main.java
@@ -3,9 +3,12 @@ package aula;
 public class main {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Meu primeiro programa em Java!");
-			
+		int idade = 24;
+		System.out.println("Minha idade é"+idade);
+		idade = 25;
+		System.out.println("Minha idade é"+idade);
+		idade = 26;
+		System.out.println("Minha idade é"+idade);
 	}
 
 }
