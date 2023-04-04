bd356cb6cb948858a2c68af093399751104ff533
Antonio Lima <antonio.lima@metatron.com.br>
2023-04-03 22:18:42 -0300

    04 - Tipos de Variáveis

--- main.java
+++ main.java
@@ -3,12 +3,25 @@ package aula;
 public class main {
 	
 	public static void main(String[] args) {
-		int idade = 24;
-		System.out.println("Minha idade é"+idade);
-		idade = 25;
-		System.out.println("Minha idade é"+idade);
-		idade = 26;
-		System.out.println("Minha idade é"+idade);
+		/*
+		 * int idade = 24; System.out.println("Minha idade é"+idade); idade = 25;
+		 * System.out.println("Minha idade é"+idade); idade = 26;
+		 * System.out.println("Minha idade é"+idade);
+		 */
+		
+		// Tipos de Variáveis
+		int numero = 24;
+	String frase = "Olá mundo";
+		boolean var = true;
+		char var2 = 'a';
+		double var3 = 24.9;
+		
+		System.out.println(numero);
+		System.out.println(frase);
+		System.out.println(var);
+		System.out.println(var2);
+		System.out.println(var3);
+		
 	}
 
 }
