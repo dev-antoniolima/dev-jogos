2c94d21ede69068ae512dba9c55c74ffca36c104
Antonio Lima <antonio.lima@metatron.com.br>
2023-04-05 18:41:48 -0300

    06 - Interfaces

---
+++ oo/PlayerInterface.java
@@ -0,0 +1,18 @@
+package oo;
+
+public interface PlayerInterface {
+
+	public abstract void iniciar() {
+		
+	}
+	
+	public abstract void morrer() {
+		
+	}
+	
+	public abstract void perderVida() {
+		
+	}
+
+}
+
