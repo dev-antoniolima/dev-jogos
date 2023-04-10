57db9c6d5da1d04a5a378e9bc56cc90e84a871ac
Antonio Lima <antonio.lima@metatron.com.br>
2023-04-10 18:18:23 -0300

    06 - Atributos e Método Construtor

--- oo/Main.java
+++ oo/Main.java
@@ -2,28 +2,20 @@ package oo;
 
 public class Main {
 	
-	private static int exemplo() {
-		return 10;
+	private String corPele = "Branca";
+	private int[] numeros;
+	public static final int VIDA_MAX = 100;
+	
+	// Método Construtor
+	public Main() {
+		System.out.println("Classe criada");
+		numeros = new int[300];
 	}
 	
-	private static String exemplo2() {
-		return "Antonio";
+	public static void main(String[] args) {
+	Main m = new Main();
+	System.out.println(m.corPele);
+	System.out.println(m.VIDA_MAX);
+		}
+	
 	}
-	
-	public void outroMetodo(int n1, String nome) {
-		System.out.println(n1);
-		System.out.println(nome);
-	}
-	
-	public static void main(String[]args) {
-		/*
-		 * new Player2().nascer(); Player2 player2 = new Player2(); player2.nascer();
-		 */
-		System.out.println(exemplo());
-		System.out.println(exemplo2());
-		
-		new Main().outroMetodo(10, "Antonio");
-}
-	
-	
-}
