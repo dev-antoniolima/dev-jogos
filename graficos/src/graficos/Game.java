458462ef5ea9458a167a88233850eeaecb95904e
Antonio Lima <antonio.lima@metatron.com.br>
2023-05-10 18:27:33 -0300

    Game Looping Profissional

--- graficos/src/graficos/Game.java
+++ graficos/src/graficos/Game.java
@@ -9,11 +9,18 @@ public class Game extends Canvas implements Runnable {
 	
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
 	private final int WIDTH = 160;
 	private final int HEIGHT = 120;
 	private final int SCALE = 3;
 	
 	public Game() {
+		
+		initFrame();
+	}
+	
+	public void initFrame() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
 		frame = new JFrame();
 		frame.add(this);
@@ -22,17 +29,69 @@ public class Game extends Canvas implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		
+	}
+	
+	
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+		
+	}
+	
+	public synchronized void stop() {
+	
+		
 	}
 	
 	public static void main(String args[]) {
 		Game game = new Game();
+		game.start();
 		
 	}
 	
+	
+	public void endCicle() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	
 	public void run() {
 		
+		long lastTime = System.nanoTime();
+		double amountOfCicles = 60.0;
+		double nanosecs = 1000000000 / amountOfCicles;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+
 		
-		
-}
+		while(isRunning) {
+
+			long now = System.nanoTime();
+			delta+= (now - lastTime) / nanosecs;
+			lastTime = now;
+				if(delta >= 1) {
+					endCicle();
+					render();
+					frames++;
+					delta--;
+				}
+				
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " +frames);
+				frames = 0;
+				timer+=1000;
+				
+			}
+		}
+			
+	}
 	
 }
